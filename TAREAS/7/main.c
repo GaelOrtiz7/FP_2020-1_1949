0b2d8bf9b4110b21151efa57d35ee1c746cdbc7c
Gael Ortiz <gael.ortiz@comunidad.unam.mx>
2019-11-07 18:59:57 -0600

    Tarea 7 corregida

--- TAREAS/7/main.c
+++ TAREAS/7/main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h> //Incluimos la librería "<math.h>", la cual nos permitirá elevar un número a determinada potencia.
+
 int main(int argC, char *argu[]){
-	long int x, y, z; //Asignamos tres variables, una para el número que vamos a elevar, otra para la potencia y otra que guardará este resultado.
-	x=atoi(argu[1]); // "x" es el número a elevar.
-	y=atoi(argu[2]); // "y" es la potencia a la que elevaremos "x".
-	z=pow(x, y); // "z" guarda el valor de "x" elevado a la "y".
-	printf("%li\n", z); //mostramos "z"
+	long int x, y, z, w; 
+	x=atoi(argu[1]); 
+	y=atoi(argu[2]); 
+	z=1;
+	w=1;
+	do{
+		w=w*x;
+		z++;
+	}while(z<=y);
+	printf("%li\n", w);
 	return 0;
 }
