352d746cfe3ac75940f07c3e38d983d88d8b07fb
Gael Ortiz <gael.ortiz@comunidad.unam.mx>
2019-11-11 22:10:17 -0600

    10

--- TAREAS/10/main.c
+++ TAREAS/10/main.c
@@ -3,7 +3,7 @@
 #include<math.h> //Incluimos la librería <math.h> que nos ayudará a realizar raíces cuadradas.
 
 int main(int argC, char *argu[]){
-	double a, b, c, y, x, x1, x2; //Declaramos las variables necesarias para resolver las ecuaciones.
+	double a, b, c, y, x, x1, x2, i; //Declaramos las variables necesarias para resolver las ecuaciones.
 	a=atof(argu[1]); //"a" tendrá el valor del primer argumento, o sea el término cuadrático de la ecuación.
 	b=atof(argu[2]); //"b" tendrá el valor del segundo argumento, o sea el término lineal de la ecuación.
 	c=atof(argu[3]); //"c" tendrá el valor del tercer argumento, o sea el término independiente de la ecuación.
@@ -20,10 +20,10 @@ int main(int argC, char *argu[]){
 	}
 	if(y<0){ // Si el determinante es negativo, las soluciones serán imaginarias, por lo tanto realizaremos lo siguiente:
 		y=(-1*y); // Multiplicamos el determinante por -1 para hacerlo positivo y poder obtener su raíz. 
-		x1=(-b+sqrt(y))/(2*a); // Después repetimos las operaciones del proceso anterior tanto para "x1" como para "x2".
-		x2=(-b-sqrt(y))/(2*a);
-		printf("%.2lf\n", x1); // Mostramos "x1".
-		printf("%.2lf\n", x2); // Mostramos "x2".
+		x=(-b)/(2*a); // Realizamos las operaciones para la parte real de la solución.
+		i=(sqrt(y))/(2*a); //Realizamos las operaciones para la parte imaginaria de la solución.
+		printf("%.2lf+%.2lfi\n", x, i); // Mostramos la primera solución, sumando la parte imaginaria. 
+		printf("%.2lf-%.2lfi\n", x, i); // Mostramos la segunda solución, restando la parte imaginaria.
 	}
 	
 	return 0;
